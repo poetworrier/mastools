e49131aca9c8f0219a00c048c3035f50e2aa3ee1
Dan <poetworrier@gmail.com>
2023-11-03 21:31:38 -0700

    tests for trends too
    tests so far are kinda lame :/
    
    add declarative format for webhooks into discord

--- api/trends_test.go
+++ api/trends_test.go
@@ -14,26 +14,32 @@ import (
 func TestListStatuses(t *testing.T) {
 	type args struct {
 		name    string
+		path    string
 		want    string
 		wantErr bool
 	}
-	tests := []args{
-		{
-			"no statuses",
-			"[]",
-			false,
-		},
-		{
-			"empty",
-			"",
-			true,
-		},
+	var tests []args
+	for _, path := range []string{"/api/v1/admin/trends/statuses", "/api/v1/admin/trends/tags"} {
+		tests = []args{
+			{
+				"no statuses",
+				path,
+				"[]",
+				false,
+			},
+			{
+				"empty",
+				path,
+				"",
+				true,
+			},
+		}
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// TODO: figure out how to make it work with TLS, Req hates the self-signed cert
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if r.URL.Path != "/api/v1/admin/trends/statuses" {
+				if r.URL.Path != tt.path {
 					w.WriteHeader(400)
 					return
 				}
